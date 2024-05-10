ba1ef6ebb757d2964ac084c2d312a160d5a0e94a
Jacob Asher - Home Desktop <31144233+Jasher1608@users.noreply.github.com>
2024-05-10 15:09:16 +1000

    Implemented player stats
    - Player stats uses scriptable object for initialisation

---
+++ Assets/_Scripts/Enums.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enums
+{
+    
+}
+
+public enum Stat
+{
+    health,
+    maxHealth,
+    maxHealthModifier,
+    meleeDamage,
+    meleeDamageModifier,
+    meleeRange,
+    meleeRangeModifier,
+    meleeCooldown,
+    meleeCooldownModifier,
+    armour,
+    armourModifier,
+    mana,
+    manaModifier,
+    magicDamage,
+    magicDamageModifier,
+    magicRange,
+    magicRangeModifier,
+    magicCooldown,
+    magicCooldownModifier,
+    luck,
+    luckModifier,
+    movementSpeed,
+    movementSpeedModifier,
+    dashRange,
+    dashRangeModifier,
+    dashCooldown,
+    dashCooldownModifier,
+    revives,
+    critChance,
+    critChanceModifier,
+    critDamage,
+    critDamageModifier,
+    abilityCooldownModifier
+}
