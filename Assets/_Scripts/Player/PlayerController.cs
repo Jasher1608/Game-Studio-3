98358e078c3b30408746e859fd4a10ab418ccf22
SLachmaiyaSAE <1031886@student.sae.edu.au>
2024-05-13 17:01:54 +1000

    Updated Player Movement to include current stats

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -6,14 +6,32 @@ public class PlayerController : MonoBehaviour
 {
     public Stats playerStats;
     private float movementSpeed;
+    private float currentHealth;
+    private float maxHealth;
+    private float meleeDamage;
+    private float meleeRange;
+    private float meleeCooldown;
+    private float armour;
+    private float mana;
+    private float magicDamage;
+    private float magicRange;
+    private float magicCooldown;
+    private float luck;
+    private float dashRange;
+    private float dashCooldown;
+    private float criticalChance;
+    private float criticalDamage;
+
+
     public Rigidbody2D rb;
     private Vector3 moveDirection;
 
     void Start()
     {
         playerStats = Instantiate(playerStats);
-        movementSpeed = PlayerStatUtils.CalculateMovementSpeed(playerStats);
+        CalculateStats();
     }
+    
     
     void Update()
     {
@@ -37,4 +55,25 @@ public class PlayerController : MonoBehaviour
     {
         rb.velocity = new Vector3(moveDirection.x * movementSpeed, moveDirection.y * movementSpeed, 0f);
     }
+
+
+
+    void CalculateStats()
+    {
+        movementSpeed = PlayerStatUtils.CalculateMovementSpeed(playerStats);
+        maxHealth = PlayerStatUtils.CalculateMaxHealth(playerStats);
+        meleeDamage = PlayerStatUtils.CalculateMeleeDamage(playerStats);
+        meleeRange = PlayerStatUtils.CalculateMeleeRange(playerStats);
+        meleeCooldown = PlayerStatUtils.CalculateMeleeCooldown(playerStats);
+        armour = PlayerStatUtils.CalculateArmour(playerStats);
+        mana = PlayerStatUtils.CalculateMana(playerStats);
+        magicDamage = PlayerStatUtils.CalculateMagicDamage(playerStats);
+        magicRange = PlayerStatUtils.CalculateMagicRange(playerStats);
+        magicCooldown = PlayerStatUtils.CalculateMagicCooldown(playerStats);
+        luck = PlayerStatUtils.CalculateLuck(playerStats);
+        dashRange = PlayerStatUtils.CalculateDashRange(playerStats);
+        dashCooldown = PlayerStatUtils.CalculateDashCooldown(playerStats);
+        criticalDamage = PlayerStatUtils.CalculateCritDamage(playerStats);
+        criticalChance = PlayerStatUtils.CalculateCritChance(playerStats);
+    } 
 }
