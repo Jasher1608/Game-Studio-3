9732596d00e04f64849073dc93b90ba2091b5911
Jasher1608 <31144233+Jasher1608@users.noreply.github.com>
2024-05-22 13:32:22 +1000

    Set up Tile Prefabs for chunk generation

---
+++ Assets/_Scripts/World Generation/TilePlacement.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilePlacement : MonoBehaviour
+{
+    void Awake()
+    {
+        transform.position = new Vector3(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y), 0);
+    }
+}
