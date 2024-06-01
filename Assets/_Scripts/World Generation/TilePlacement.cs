e0a542e1bd58a7aea3d6dc1eeed6a4beff3e07fe
Jacob Asher - Home Desktop <31144233+Jasher1608@users.noreply.github.com>
2024-06-01 20:53:05 +1000

    Fixed a bug with tile placement

--- Assets/_Scripts/World Generation/TilePlacement.cs
+++ Assets/_Scripts/World Generation/TilePlacement.cs
@@ -7,5 +7,6 @@ public class TilePlacement : MonoBehaviour
     void Awake()
     {
         transform.position = new Vector3(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y), 0);
+        transform.localScale = Vector3.one;
     }
 }
