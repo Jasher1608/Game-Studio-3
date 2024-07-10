6d032e42b306e9ba3355184e19a3c2c5ea5e5287
SLachmaiyaSAE <1031886@student.sae.edu.au>
2024-07-11 00:43:40 +1000

    Main Menu Initial Build

---
+++ Assets/_Scripts/MainMenu/MainMenuEvents.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MainMenuEvents : MonoBehaviour
+{
+    private UIDocument _document;
+    private Button _button;
+    private List<Button> _menuButtons = new List<Button>();
+    private AudioSource _audioSource;
+
+
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        _document = GetComponent<UIDocument>();
+
+        _button = _document.rootVisualElement.Q("Play") as Button;
+        _button.RegisterCallback<ClickEvent>(OnPlayClick);
+
+        _menuButtons = _document.rootVisualElement.Query<Button>().ToList();
+        for (int i = 0; i < _menuButtons.Count; i++)
+        {
+            _menuButtons[i].RegisterCallback<ClickEvent>(OnAllButtonClick);
+        }
+
+    }
+
+    private void OnDisable()
+    {
+        _button.UnregisterCallback<ClickEvent>(OnPlayClick);
+
+        for (int i = 0; i < _menuButtons.Count; i++)
+        {
+            _menuButtons[i].UnregisterCallback<ClickEvent>(OnAllButtonClick);
+        }
+    }
+
+    private void OnPlayClick(ClickEvent evt)
+    {
+        Debug.Log("You pressed the Play Button");
+        // insert load next scene here / action here
+    }
+
+    private void OnSettingsClick(ClickEvent evt)
+    {
+        // insert settings panel functionality
+    }
+
+    private void OnControlsCLick(ClickEvent evt)
+    {
+        // insert Controls panel functionality
+    }
+
+    private void OnAllButtonClick(ClickEvent evt)
+    {
+        _audioSource.Play();
+    }
+
+}
