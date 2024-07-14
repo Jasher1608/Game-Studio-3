536a1684cdb286cfa2b585260e8d0582c2aa195e
SLachmaiyaSAE <1031886@student.sae.edu.au>
2024-07-14 23:28:19 +1000

    troubleshooting stylesheets for panel
    Text not visible - yet.

--- Assets/_Scripts/MainMenu/PopUpSettingsPanel.cs
+++ Assets/_Scripts/MainMenu/PopUpSettingsPanel.cs
@@ -4,26 +4,46 @@ using UnityEngine.UIElements;
 
 namespace PopupSettingsPanel
 {
+
     public class PopupWindow : VisualElement
     {
         [UnityEngine.Scripting.Preserve]
         public new class UxmlFactory : UxmlFactory<PopupWindow>
         {
-
+            
         }
-
-
 
         private const string styleResource = "PopupWindowStyleSheet";
         private const string ussPopup = "popup_window";
+        private const string ussPopupContainer = "popup_container";
+        private const string ussHorContainer = "horizontal_container";
+        private const string ussPopupMsg = "popup_msg";
 
         public PopupWindow()
         {
             styleSheets.Add(Resources.Load<StyleSheet>(styleResource));
+            AddToClassList(ussPopupContainer);
 
             VisualElement window = new VisualElement();
+            //window.style.backgroundColor = new StyleColor(Color.blue); // Temporary style for visibility
             window.AddToClassList(ussPopup);
             hierarchy.Add(window);
+
+            // Text Section
+            VisualElement horizontalContainerText = new VisualElement();
+           // horizontalContainerText.style.backgroundColor = new StyleColor(Color.green); // Temporary style for visibility
+            horizontalContainerText.AddToClassList(ussHorContainer);
+            window.Add(horizontalContainerText);
+
+            Label msgLabel = new Label();
+            Debug.Log("reached message label");
+
+            msgLabel.text = "Do you really want the red pill?";
+            //msgLabel.style.color = Color.yellow; // Temporary style for visibility
+            msgLabel.AddToClassList(ussPopupMsg);
+            horizontalContainerText.Add(msgLabel);
+
+            
         }
 
     }
